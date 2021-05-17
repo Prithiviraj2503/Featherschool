a09f49a90c31a0bfa5df21089ab9c173fa2c8bd0
PRITHIVIRAJ K <prithivirajk2503@gmail.com>
2021-05-17 09:55:13 +0530

    Update editstudentreport.php

--- include/templates/editstudentreport.php
+++ include/templates/editstudentreport.php
@@ -1,5 +1,4 @@
-
-   <?php
+<?php
 $mscid=0;
 if(isset($_GET['msc']))
 {
@@ -69,3 +68,44 @@ if($mscid==3)
                 </thead>		
             </table>
 								
+<canvas id="totalstudentschart" width="100%" height="50%"></canvas>
+<script src="chartjs.js"></script>
+<script>
+    var totallkg   = <?php if(isset($totallkg)){echo $totallkg; } ?>;
+    var totalukg   = <?php if(isset($totalukg)){echo $totalukg; } ?>;
+    var totalI     = <?php if(isset($totalI)){echo $totalI; } ?>;
+    var totalII    = <?php if(isset($totalII)){echo $totalII; } ?>;
+    var totalIII   = <?php if(isset($totalIII)){echo $totalIII; } ?>;
+    var totalIV    = <?php if(isset($totalIV)){echo $totalIV; } ?>;
+    var totalV     = <?php if(isset($totalV)){echo $totalV; } ?>;
+    var totalVI    = <?php if(isset($totalVI)){echo $totalVI; } ?>;
+    var totalVII   = <?php if(isset($totalVII)){echo $totalVII; } ?>;
+    var totalVIII  = <?php if(isset($totalVIII)){echo $totalVIII; }?>;
+    var totalIX    = <?php if(isset($totalXI)){echo $totalXI; } ?>;
+    var totalX     = <?php if(isset($totalX)){echo $totalX; } ?>;
+    var totalXI    = <?php if(isset($totalXI)){echo $totalXI; } ?>;
+    var totalXII   = <?php if(isset($totalXII)){echo $totalXII; }?>; 
+
+new Chart(document.getElementById("totalstudentschart"), {
+    type: 'bar',
+    data: {
+      labels: ["LKG","UKG","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII"],
+      datasets: [
+        {
+          label: "",
+          backgroundColor: ["#ff0000", "#ff8000","#ffff00","#00ff80","#00ff00",
+          "#00bfff","#ff0080","#8000ff","#0000ff","#00cca3","#ff0080","#b37700","#99cc00","#408000"],
+          data: [totallkg, totalukg, totalI, totalII, totalIII, totalIV, totalV, totalVI, totalVII, totalVIII, totalIX, totalX, totalXI, totalXII]
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'Number Of Students'
+      }
+    }
+});
+
+</script>
